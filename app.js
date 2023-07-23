0abb533f7d70219a49b188f181a014061eff86a3
agstartec <agstartec@gmail.com>
2023-07-23 15:10:48 -0300

    feat: atualização de conexão com banco de dados

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
-
+const { Sequelize } = require('sequelize');
 let app = express();
+
+const sequelize = new Sequelize('u853547394_zplay', 'u853547394_zplayUser', 'nix8T2DHnJG4rssb', {
+  host: '89.117.7.166',
+  dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
+
+
+app.get('/',async (req,res)=>{
+    try {
+        await sequelize.authenticate();
+        res.status(200).json('{"status":"Ok"}');
+      } catch (error) {
+        res.status(307).json('{"status":"Não autorizado"}');
+    }
+   
+})
 
 app.listen(3000,()=>{
     console.log("rodando");
